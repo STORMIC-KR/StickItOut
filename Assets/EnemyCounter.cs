b64854352d7c3918e85d22ce6673ce2fc2d21bec
STORMIC-KR <gyb4019@gmail.com>
2021-06-29 21:17:19 +0900

    2nd Upload
    적 수 카운팅 시스템 추가

---
+++ Assets/EnemyCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCounter : MonoBehaviour
+{
+    GameObject[] enemies;
+    public Text enemyCountText;
+
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        enemyCountText.text = "Enemy : " + enemies.Length.ToString() + " Left...";
+    }
+}
