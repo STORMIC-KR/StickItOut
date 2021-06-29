3ac39d40d534232cd7b7a33ecaeb59c9d5d3dc2c
STORMIC-KR <53865298+STORMIC-KR@users.noreply.github.com>
2021-06-29 14:44:06 +0900

    4th Upload
    랜덤 레벨 기능 추가

---
+++ Assets/LoadGameLevels.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameLevels : MonoBehaviour
+{
+
+    public void LoadRandomScene()
+    {
+        int index = Random.Range(1, 4);
+        SceneManager.LoadScene(index);
+        Debug.Log("Scene Loaded");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadRandomScene();
+        }
+    }
+}
